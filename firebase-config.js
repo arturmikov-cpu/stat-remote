6b06b29c4a29c35fb37177d8ac4c9c48d65b85b3
arturmikov-cpu <arturmikov@gmail.com>
2026-08-20 16:26:20 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxRWPH50Jl-K290hLikKZAA41qeuP8s9c",
+  authDomain: "stat-remote.firebaseapp.com",
+  databaseURL: "https://stat-remote-default-rtdb.firebaseio.com",
+  projectId: "stat-remote",
+  storageBucket: "stat-remote.firebasestorage.app",
+  messagingSenderId: "702880337194",
+  appId: "1:702880337194:web:fc1b4db5fdfbbdb9416253",
+  measurementId: "G-7S554X0ZVQ"
+};
